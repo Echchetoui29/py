09e90515252b189890afc5931658fa0d6caf4070
Echchetoui29 <t.echchetoui2914@uca.ac.ma>
2025-08-04 11:29:09 +0200

    update

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, render_template, request, redirect, url_for, flash
-from logic import find_py_files, send_file_to_telegram
+from api.logic import find_py_files, send_file_to_telegram
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
